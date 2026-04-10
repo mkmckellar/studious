bfa77b587d8a9a6b35d0fd3c87404a2a66d31a28
mkmckellar <112785237+mkmckellar@users.noreply.github.com>
2026-04-10 10:04:30 -0700

    Remove placeholder files and test artifacts, add scratch/ to ignore files

--- R/fxn_test_1.R
+++
@@ -1,9 +0,0 @@
-fxn1 <- function(num1, num2){
-  # a simple function that checks if two elements are numeric
-  # and adds them together, else returns error message
-  if(is.numeric(num1) & is.numeric(num2)){
-    return(num1 + num2)
-  }else{
-    print("One or both elements are not numeric.")
-  }
-}
