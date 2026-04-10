ee6b75e27d776a1ae7ec3ac96887edaadd046571
mkmckellar <112785237+mkmckellar@users.noreply.github.com>
2026-04-10 09:51:00 -0700

    Initial commit

---
+++ R/create_homework_html.R
@@ -0,0 +1,48 @@
+# create homework html template
+
+
+# this code was copied from meghan hall's blog post
+create_homework_html <- function(file_name = NULL,
+                              ext_name = "hw-html") {
+
+  # first check if file_name exists, ask user to name file if not
+  if (is.null(file_name)) {
+    stop("You must provide a valid file_name")
+  }
+
+  # check for available extensions
+  stopifnot("Extension not in package" = ext_name %in% c("hw-html"))
+
+  # check for existing _extensions directory
+  if(!file.exists("_extensions")) dir.create("_extensions")
+  message("Created '_extensions' folder")
+
+  # create folder
+  if(!file.exists(paste0("_extensions/", ext_name))) dir.create(paste0("_extensions/", ext_name))
+
+  # copy from internals, packaged referenced is this package -- studious!
+  file.copy(
+    from = system.file(paste0("extdata/_extensions/", ext_name), package = "studious"),
+    to = paste0("_extensions/"),
+    overwrite = TRUE,
+    recursive = TRUE,
+    copy.mode = TRUE
+  )
+
+  # logic check to make sure extension files were moved
+  n_files <- length(dir(paste0("_extensions/", ext_name)))
+
+  if(n_files >= 2){
+    message(paste(ext_name, "was installed to _extensions folder in current working directory."))
+  } else {
+    message("Extension appears not to have been created")
+  }
+
+  # create new qmd hw template based on skeleton
+  file.copy("_extensions/hw-html/hw-html-template.qmd",
+            paste0(file_name, ".qmd", collapse = ""))
+
+  # open the new file in the editor
+  file.edit(paste0(file_name, ".qmd", collapse = ""))
+
+}
